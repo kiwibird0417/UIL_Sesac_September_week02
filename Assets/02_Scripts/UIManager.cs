f78f030f92352d5700cbf8e2123cca281486c65a
kiwibird0417 <kiwibird0417@gmail.com>
2024-09-06 11:29:18 +0900

    standard player upload
    standard player template upload
    - Title Scene
    - Sample Scene

---
+++ Assets/02_Scripts/UIManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+//08.23(금)
+public class UIManager : MonoBehaviour
+{
+    // 버튼을 할당할 변수 선언
+    public Button startButton;
+
+
+    void OnEnable()
+    {
+        // 이벤트 연결
+        startButton.onClick.AddListener(() => OnStartButtonClick());
+    }
+
+
+    public void OnStartButtonClick()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+}
